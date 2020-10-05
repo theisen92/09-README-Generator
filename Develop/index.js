a60d4c1a565f36ed7431024d207de5d281539f45
theisen92 <andrew.peter.theisen@gmail.com>
2020-10-05 18:01:37 -0500

    basic functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,68 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What your README title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the description you want to inclued in your README",
+    name: "descript",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "What are the steps for usage?",
+    name: "use",
+  },
+  {
+    type: "list",
+    message: "Select a license for your application.",
+    name: "license",
+    choices: ["Apache", "ISC", "Mozilla", "Perl", "Unlicense"],
+  },
+  {
+    type: "input",
+    message: "Any contributers to credit?",
+    name: "credit",
+  },
+  {
+    type: "input",
+    message: "What tests do you have for your application?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: `In case someone what's to reach out for additional questions,
+      What is your email address?`,
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "    What is your GitHub username?",
+    name: "git",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function (answer) {
+    var mD = generateMarkdown(answer);
+    console.log(mD);
+  });
+}
 
 // function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,85 @@
 // function to generate markdown for README
+// Images and URLs grabbed from - https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 function generateMarkdown(data) {
-  return `# ${data.title}
+  var license = "";
+  var bImg = "";
+  var bURL = "";
+  // the license choice
+  // Images and URLs grabbed from - https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+  switch (data.license) {
+    case "Apache":
+      license = "";
+      bImg = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+      bURL = "https://opensource.org/licenses/Apache-2.0";
+      break;
 
+    case "ISC":
+      license = "ISC";
+      bImg = "https://img.shields.io/badge/License-ISC-blue.svg";
+      bURL = "https://opensource.org/licenses/ISC";
+      break;
+
+    case "Mozilla":
+      license = "MPL 2.0";
+      bImg = "https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg";
+      bURL = "https://opensource.org/licenses/MPL-2.0";
+      break;
+
+    case "Perl":
+      license = "Artistic-2.0";
+      bImg = "https://img.shields.io/badge/License-Perl-0298c3.svg";
+      bURL = "https://opensource.org/licenses/Artistic-2.0";
+      break;
+
+    case "Unlicense":
+      license = "Unlicense";
+      bImg = "https://img.shields.io/badge/license-Unlicense-blue.svg";
+      bURL = "http://unlicense.org/";
+      break;
+  }
+
+  return `#${data.title}
+  #Discription
+
+  ${data.descript}
+
+  #Table of Contents 
+
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+  #Installation
+
+  ${data.install}
+
+  #Usage
+
+  ${data.use}
+
+  #License
+
+  [![License: ${license}](${bImg})](${bURL})
+
+  #Contributing
+
+  ${data.credit}
+
+  #Tests
+
+  ${data.test}
+
+  #Questions
+
+  If you have any additional questions,
+  Please reach out to me by email or through Github
+
+  Email: ${data.email}
+
+  Github: ${data.git}
 `;
 }
 
